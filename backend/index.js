327daa817be47132c28a079f30b8eb55a30cadec
Vagner H <vagner_h3rd7@outlook.com>
2020-03-29 12:36:21 -0300

    24/03 params

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,30 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET    :  Buscar/listar uma informação do back-end
+ * POST   :  Criar uma informação no back-end
+ * PUT    :  Alterar uma informação no back-end
+ * DELETE :  Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params : Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ *   http://localhost:3333/users?name=Vagner
+ *   http://localhost:3333/users?page=2&name=Vagner&age=26
+ */
+
+app.get('/users', (request, response) => {
+    const params =request.query;
+    console.log(params);
 
     return response.json({
         evento: 'Semana OmniStack 11.0',
